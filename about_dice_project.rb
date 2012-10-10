5e6f8394d0ced23e7a4582787b4b04cfee72569d
Corinna Brock <corinna.brock@gmail.com>
2012-10-10 11:37:07 -0400

    commented dice project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,15 +6,15 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #   code ...
 # end
 class DiceSet
-  attr_reader :values
+  attr_accessor :values
+
   def roll(number_of_die)
-    
     @values = []
-    
-    number_of_die.downto(1) 
-    { |i| @values.push(1 + rand(6))}
+    number_of_die.times { @values << rand(6) + 1 }
+    #need to add one so the numbers are correct
   end
 end
+
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
